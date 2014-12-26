4dba0bdebbfb6764596466b01f000a0b71093e2b
Gabriele O'Connor <oconnor.gabriele@gmail.com>
2014-12-26 17:04:57 -0600

    Added HTML elements

---
+++ application.js
@@ -0,0 +1,28 @@
+// select grid size
+
+// select grid color(s)
+
+// generate number of grids
+
+// 
+
+
+
+$(document).ready(function() {
+	
+	//run this function on hover
+	var draw_on_grid = function(color) {
+	$('grid').on('hover', function()
+	
+	)};
+
+	$(this).css()
+
+	//event delegation
+
+var get_random_color = function() {
+  var r = function() { return Math.floor(Math.random()*256) };
+  return "rgb(" + r() + "," + r() + "," + r() + ")";
+}
+
+})
