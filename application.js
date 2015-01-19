4dc2ac68792a78bc1291d9a9333a84cbba5853b5
GabiOC <oconnor.gabriele@gmail.com>
2015-01-19 17:12:40 -0500

    Add click handler and grid creation function

--- application.js
+++ application.js
@@ -1,14 +1,49 @@
 $(document).ready(function(){
-	
-// Triggers prompt to input grid size
-	$("#size").click(function(){
-		prompt("Enter a number between 0 and 100");
+	var dimension = 16; // default size of grid
+	var apiCats = 'src="http://thecatapi.com/api/images/get?&format=src&type=gif&size=small"';
+	var whichButton = parseInt($(this).attr("value"));
+
+	// Handles click, checks input validity and triggers new grid
+	$(".button").click(function(){
+		$(".grid_sq").remove();
+		var dimension = parseInt(prompt("Enter a number 1-100 for grid. 'Return' for default."));
+		if(isNaN(dimension) || dimension <1 || dimension >100){
+			dimension = ((whichButton === 4) ? 5 : 16) // smaller grid default for cat button
+		};
+		createGrid(dimension);
+		handle(whichButton);
 	});
 
-// Triggers color options dropdown 
-	$(".color-options").hide();
-	$("#color").click(function(){
-		$(".color-options").slideToggle("slow");
-	});
+	// Creates new grid
+	function createGrid(dimension){
+		// Set grid size
+		var boxNum = dimension * dimension;
+		var boxSize = (960)/dimension;
+		var i = 0;
+
+		// Make new grid
+		while(i<boxNum){
+			i++;
+			var $grid = $('<div class="grid_sq"></div>');
+			$(".container").append($grid);
+				($grid).css({"width": boxSize + "px", "height": boxSize + "px"});
+		}
+	};
+
+	// Function effects
+	var handle = function(whichButton){
+		$(".container").find(".grid_sq").on("mouseenter", function(){
+			switch(whichButton){
+				case 1: $(this).css({"background-color": "white"}); break;
+				case 2: $(this).css({"background-color": "#"+Math.floor(Math.random()*16777215).toString(16)}); break;
+				case 3: 
+					var currentOpacity = $(this).css("opacity")
+					if(currentOpacity != 0){
+						$(this).css("opacity", currentOpacity - 0.10)
+					}; break;
+				case 4: $(this).append($(this).append('<img width="' + boxSize + '" height="' + boxSize + '" ' + apiCats + ' >')); break;
+			};
+		});
+	};
 });
 
