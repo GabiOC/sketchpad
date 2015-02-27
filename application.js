c1e7941a052e562b5520f073a1ef19c2911fc57b
GabiOC <oconnor.gabriele@gmail.com>
2015-02-26 20:36:54 -0500

    Add default grid and effect on page load

--- application.js
+++ application.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
 	var newGrid;
 
+	// load default grid and effect
+	createGrid(16);
+	$(".container").find(".grid_sq").on("mouseenter", function(){
+		$(this).css({"background-color": "white"});
+	});
+
 	$("button").click(function(event){
 		var buttonId = event.target.id;
 		$(".grid_sq").remove(); // removes previous grid
@@ -13,11 +19,11 @@ $(document).ready(function(){
 
 		// sets hover effect
 		$(".container").find(".grid_sq").on("mouseenter", function(){
-			if (buttonId=="white"){
+			if (buttonId == "white"){
 				$(this).css({"background-color": "white"});
 			}
-			else if (buttonId=="random"){
-				$(this).css({"background-color": '#' + Math.random().toString(16).substring(2, 8)});
+			else if (buttonId == "random"){
+				$(this).css({"background-color": '#' + Math.random().toString(16).substring(2,8)});
 			}
 			else{
 				var boxSize = Math.floor(960)/newGrid;
@@ -33,7 +39,7 @@ $(document).ready(function(){
 		var i = 0;
 
 		// creates grid divs
-		while(i<=boxNum){
+		while(i <= boxNum){
 			i++;
 			var $grid = $('<div class="grid_sq"></div>');
 			$(".container").append($grid);
@@ -45,4 +51,5 @@ $(document).ready(function(){
 	setInterval(function(){
     	$(".grid_sq img").attr("src", "http://thecatapi.com/api/images/get?&format=src&type=gif&size=small&" + new Date().getTime());
     },2000);
+
 });
